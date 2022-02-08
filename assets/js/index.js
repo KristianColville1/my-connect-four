0201ee8ad887d0a22908d54fe30ca10137d5e1bb
Kristian Philip Colville <kristiancolville1@gmail.com>
2022-02-08 14:09:43 +0000

    Fix loop bug and configure live server

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,27 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayCurrentPlayer = document.querySelector('#current-player');
     let currentPlayer = 1;
 
-    for(let i = 0, len = squares.length; i < len; i < len){
-        // Add a onclick function to each quare in the grid
-        squares[i].onclick = function(){
-            // if the square below the current square is taken add square on top
-            if(squares[index + 7].classList.contains('taken')){
-                if(currentPlayer === 1){
-                    squares[index].classList.add('taken');
-                    squares[index].classList.add('player-one');
-                    // change the player then after player one goes
-                    currentPlayer = 2;
-                    displayCurrentPlayer.innerHTML = currentPlayer;
-                } else if(currentPlayer === 2){
-                    squares[index].classList.add('taken');
-                    squares[index].classList.add('player-two');
-                    // change the player back to player one after player two goes
+    for(var i = 0, len = squares.length; i < len; i++){
+        (function(index){
+            // Add a onclick function to each quare in the grid
+            squares[i].onclick = function(){
+                // if the square below the current square is taken add square on top
+                if(squares[index + 7].classList.contains('taken')){
+                    if(currentPlayer === 1){
+                        squares[index].classList.add('taken');
+                        squares[index].classList.add('player-one');
+                        // change the player then after player one goes
+                        currentPlayer = 2;
+                        displayCurrentPlayer.innerHTML = currentPlayer;
+                    } else if(currentPlayer === 2){
+                        squares[index].classList.add('taken');
+                        squares[index].classList.add('player-two');
+                        // change the player back to player one after player two goes
+                        currentPlayer = 1;
+                        displayCurrentPlayer.innerHTML = currentPlayer;
+                    }
+                } else {
+                    alert('You cannot go there..');
                 }
             }
-        }
+        })(i);
     }
-
-
-
-
 });
